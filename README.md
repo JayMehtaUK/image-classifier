49ca62b2c403a66a8fd9da15da9636537c85936e
Jay <jay_mehta@hotmail.co.uk>
2023-07-10 23:09:02 +0100

    add initial code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # image-classifier
+
+## Prerequisites 
+To install dependencies run the following command 
+``` bash
+pip install -r requirements.txt
+```
+
+## Running Locally
+To run locally either run from the IDE or use the following command:
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+#from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+
+app = Flask(__name__)
+model = ResNet50()
+
+@app.route('/', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile= request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    image = load_img(image_path, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label = label[0][0]
+
+    classification = '%s (%.2f%%)' % (label[1], label[2]*100)
+
+
+    return render_template('index.html', prediction=classification)
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
